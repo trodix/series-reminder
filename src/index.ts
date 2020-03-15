990a8a2831e606e74e7fecd2cb84779a150c21ca
Sébastien Vallet <sebastien.vallet89@gmail.com>
2020-03-15 21:26:21 +0100

    rework app architecture

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express, { Application, Request, Response } from 'express';
 import schedule from 'node-schedule';
 import { checkForNewSeries } from './scraper';
+import path from 'path';
 
 const app: Application = express();
 const PORT: number = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('ok');
-});
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('ok');
+// });
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}...`);
